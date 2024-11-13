5698d5b012ffcb8521ec94522ee234073f883f39
medha-14 <medha140904@gmail.com>
2024-11-14 03:05:32 +0530

    exit class

--- game.py
+++ game.py
@@ -284,6 +284,14 @@ class Water(pygame.sprite.Sprite):
 		self.rect.midtop = (x + TILE_SIZE // 2, y + (TILE_SIZE - self.image.get_height()))
 
 
+class Exit(pygame.sprite.Sprite):
+	def __init__(self, img, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = img
+		self.rect = self.image.get_rect()
+		self.rect.midtop = (x + TILE_SIZE // 2, y + (TILE_SIZE - self.image.get_height()))
+
+
 
 class ItemBox(pygame.sprite.Sprite):
 	def __init__(self, item_type, x, y):
